7d9dec4207adac2ddfcf35ebc4b5970199f3cad9
Tiemoko <mtiecamou@gmail.com>
2020-12-10 14:46:20 +0100

    add bootstrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,9 +2,7 @@
 <html>
 <head>
     <title>{{ config('app.name', 'Laravel') }} - @yield('title')</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"></head>
 <body>
 @section('navbar')
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -49,25 +47,9 @@
     </nav>
 @show
 
-<div class="container-fluid">
-    <div class="starter-template">
-        <div class="row">
-            <div class="col-12">
-                @yield('content')
-            </div>
-        </div>
-
+    <div id="app" class="w-100 h-100">
+        @yield('content')
     </div>
-</div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+<script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
